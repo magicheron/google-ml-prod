0a59910839eda09082bf632711a1fbd559dc22aa
Magicheron <magicheron@gmail.com>
2021-01-29 18:39:39 +0000

    Update predict to ai-platform

--- bin/predict.sh
+++ bin/predict.sh
@@ -3,7 +3,7 @@ INPUT_DATA_FILE="data/instances.json"
 VERSION_NAME="v0_1"
 REGION="europe-west1"
 
-gcloud ml-engine predict --model $MODEL_NAME \
+gcloud ai-platform predict --model $MODEL_NAME \
 --version $VERSION_NAME \
 --json-instances $INPUT_DATA_FILE \
 --region $REGION

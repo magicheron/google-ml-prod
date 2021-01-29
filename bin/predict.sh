03bdadc9492f9a1297360cd0b5eb6efb30f9ed9d
Magicheron <magicheron@gmail.com>
2021-01-29 18:38:30 +0000

    Update predict

--- bin/predict.sh
+++ bin/predict.sh
@@ -2,3 +2,8 @@ MODEL_NAME="clase_29012021"
 INPUT_DATA_FILE="data/instances.json"
 VERSION_NAME="v0_1"
 REGION="europe-west1"
+
+gcloud ml-engine predict --model $MODEL_NAME \
+--version $VERSION_NAME \
+--json-instances $INPUT_DATA_FILE \
+--region $REGION
